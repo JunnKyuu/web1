67962015cdbbabed441532c2864c4085dbef93f1
JunnKyuu <junnkyuu22@gmail.com>
2023-05-20 13:00:09 +0900

    body overflow hidden 및 second article 구현

--- basic_web/study/practice_web/cow_BankSalad/banksalad.js
+++ basic_web/study/practice_web/cow_BankSalad/banksalad.js
@@ -3,18 +3,22 @@ const $button1 = document.getElementById('button1');
 const $button2 = document.getElementById('button2');
 const $button3 = document.getElementById('button3');
 let count = 1;
-$button1.addEventListener('click', (evt) => {
+$button1.addEventListener('click', () => {
+    // carousel 1번 버튼 클릭 이벤트
     clickButton1();
 });
-$button2.addEventListener('click', (evt) => {
+$button2.addEventListener('click', () => {
+    // carousel 2번 버튼 클릭 이벤트
     clickButton2();
 });
 
-$button3.addEventListener('click', (evt) => {
+$button3.addEventListener('click', () => {
+    // carousel 3번 버튼 클릭 이벤트
     clickButton3();
 });
 
 const clickButton1 = () => {
+    // carousel 1번 버튼 함수
     $container.style.transform = 'translateX(0)';
     $button1.style.backgroundColor = '#dddddd';
     $button2.style.backgroundColor = '#ffffff';
@@ -22,6 +26,7 @@ const clickButton1 = () => {
 }
 
 const clickButton2 = () => {
+    // carousel 2번 버튼 함수
     $container.style.transform = 'translateX(-100vw)';
     $button2.style.backgroundColor = '#dddddd';
     $button1.style.backgroundColor = '#ffffff';
@@ -29,6 +34,7 @@ const clickButton2 = () => {
 }
 
 const clickButton3 = () => {
+    // carousel 3번 버튼 함수
     $container.style.transform = 'translateX(-200vw)';
     $button3.style.backgroundColor = '#dddddd';
     $button1.style.backgroundColor = '#ffffff';
@@ -36,6 +42,7 @@ const clickButton3 = () => {
 }
 
 setInterval(() => {
+    // 자동슬라이드를 위한 setInterval 함수
     if(count === 1) {clickButton2();}
     if(count === 2) {clickButton3();}
     if(count === 3) {clickButton1(); count = 0;}
