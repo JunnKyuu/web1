12b23b7e1c044e076874e7d4f807289e3566d850
JunnKyuu <junnkyuu22@gmail.com>
2023-05-22 18:12:15 +0900

    web1 project - login 구현

---
+++ basic_web/web1/term_project/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+>
+login/login.js,e/7/e7a1fdd9128afad5cc21a71154bcd3b232166d80
+?
+login/login.css,f/1/f1b59123c4dad90283af054808d89addbfcebeb0
+@
+login/login.html,7/1/71bad88367e10555cd4091c4fc5ab607b41695a8
---
+++ basic_web/web1/term_project/login/login.js
@@ -0,0 +1,44 @@
+const $loginWrap = document.getElementById('login-wrap');
+const $loginClose = document.getElementById('login-close');
+const $loginBtn = document.getElementById('login-btn');
+const $logoutBtn = document.getElementById('logout-btn');
+const $loginId = document.getElementById('id');
+const $loginPw = document.getElementById('pw');
+const $loginSubmit = document.getElementById('info-submit');
+const userId = '';
+const userPw = '';
+
+$loginBtn.addEventListener('click', () => {
+  $loginWrap.style.display = 'block';
+});
+
+$loginClose.addEventListener('click', () => {
+  $loginWrap.style.display = 'none';
+});
+
+$loginSubmit.addEventListener('click', () => {
+  const userId = $loginId.value;
+  const userPw = $loginPw.value;
+
+  if((userId === '') || (userPw === '')) {
+    alert('아이디, 비밀번호를 정확하게 입력해주세요.');
+    $loginWrap.style.display = 'block';
+  } else {
+    $loginBtn.style.display = 'none';
+    $logoutBtn.style.display = 'block';
+    $loginWrap.style.display = 'none';
+
+    alert(userId + '님 안녕하세요!');
+  }
+
+  $loginId.value = null;
+  $loginPw.value = null;
+});
+
+$logoutBtn.addEventListener('click', () => {
+  if(confirm('로그아웃 하시겠습니까?') == true) {
+    $loginWrap.style.display = 'block';
+    $loginBtn.style.display = 'block';
+    $logoutBtn.style.display = 'none';
+  } 
+});
