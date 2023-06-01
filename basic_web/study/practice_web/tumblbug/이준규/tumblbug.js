2faf3303d8e54a5d260eb5192bda0408730618d0
JunnKyuu <junnkyuu22@gmail.com>
2023-06-02 00:03:10 +0900

    login, divider, pagination 구현

--- basic_web/study/practice_web/tumblbug/이준규/tumblbug.js
+++ basic_web/study/practice_web/tumblbug/이준규/tumblbug.js
@@ -1,6 +1,7 @@
 const $rightBtn = document.getElementById('carousel-btn-right');
 const $leftBtn = document.getElementById('carousel-btn-left');
 const $carousel = document.getElementById('carousel');
+const $pagination = document.getElementById('pagination');
 let currentSlide = 1;
 
 $rightBtn.addEventListener('click', () => {
@@ -16,18 +17,23 @@ $leftBtn.addEventListener('click', () => {
 const clickRight = () => {
     if(currentSlide === 1) {
         $carousel.style.transform = 'translateX(-20%)';
+        $pagination.innerHTML = '<p>2/5</p>';
     }
     if(currentSlide === 2) {
         $carousel.style.transform = 'translateX(-40%)';
+        $pagination.innerHTML = '<p>3/5</p>';
     }
     if(currentSlide === 3) {
         $carousel.style.transform = 'translateX(-60%)';
+        $pagination.innerHTML = '<p>4/5</p>';
     }
     if(currentSlide === 4) {
         $carousel.style.transform = 'translateX(-80%)';
+        $pagination.innerHTML = '<p>5/5</p>';
     }
     if(currentSlide === 5) {
         $carousel.style.transform = 'translateX(0)';
+        $pagination.innerHTML = '<p>1/5</p>';
         currentSlide = 0;
     }
 }
@@ -35,23 +41,28 @@ const clickRight = () => {
 const clickLeft = () => {
     if(currentSlide === 1) {
         $carousel.style.transform = 'translateX(-80%)';
+        $pagination.innerHTML = '<p>5/5</p>';
         currentSlide = 6;
     }
     if(currentSlide === 2) {
         $carousel.style.transform = 'translateX(0)';
+        $pagination.innerHTML = '<p>1/5</p>';
     }
     if(currentSlide === 3) {
         $carousel.style.transform = 'translateX(-20%)';
+        $pagination.innerHTML = '<p>2/5</p>';
     }
     if(currentSlide === 4) {
         $carousel.style.transform = 'translateX(-40%)';
+        $pagination.innerHTML = '<p>3/5</p>';
     }
     if(currentSlide === 5) {
         $carousel.style.transform = 'translateX(-60%)';
+        $pagination.innerHTML = '<p>4/5</p>';
     }
 }
 
-// setInterval(() => {
-//     clickRight();
-//     currentSlide ++;
-// }, 4000);
+setInterval(() => {
+    clickRight();
+    currentSlide ++;
+}, 4000);
