19ad5f371486cafcb15ffd9a9d6dbf99f70a5bca
JunnKyuu <junnkyuu22@gmail.com>
2023-06-19 00:33:32 +0900

    회원가입 js 추가

---
+++ basic_web/web1/term_project/signUp/signUp.js
@@ -0,0 +1,15 @@
+const $submit = document.querySelector('#submit');
+
+$submit.addEventListener('click', () => {
+  let $email = document.querySelector('#email');
+  let $password = document.querySelector('#password');
+  let $nickName = document.querySelector('#nick-name');
+  let $address = document.querySelector('#address');
+  let $phoneNumber = document.querySelector('#phone-number');
+});
+
+console.log($email);
+console.log($password);
+console.log($nickName);
+console.log($address);
+console.log($phoneNumber);
