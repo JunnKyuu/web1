215ccc2e9b6e97cc967496edfda8802745e446bc
JunnKyuu <junnkyuu22@gmail.com>
2023-06-20 00:45:58 +0900

    signUp 완성

--- basic_web/web1/term_project/signUp/signUp.js
+++ basic_web/web1/term_project/signUp/signUp.js
@@ -1,5 +1,27 @@
 const $submit = document.querySelector('#submit');
 
+let userId = null;
+let userPassword = null;
+let userNickName = null;
+let userAddress = null;
+let userPhoneNumber = null;
+
+let data = new XMLHttpRequest();
+data.open('GET', './account.json', true);
+data.responseType = 'json';
+data.onload = function() {
+  if (data.status === 200) {
+    let jsondata = data.response;
+
+    userId = jsondata.email;
+    userPassword = jsondata.password;
+    userNickName = jsondata.nickName;
+    userAddress = jsondata.address;
+    userPhoneNumber = jsondata.phoneNumber;
+  }
+};
+data.send();
+
 $submit.addEventListener('click', () => {
   const $email = document.querySelector('#email').value;
   const $password = document.querySelector('#password').value;
@@ -12,19 +34,26 @@ $submit.addEventListener('click', () => {
     icon: 'error',
     title: '입력 오류',
     text: '정보를 빠짐없이 정확히 입력해주세요!',
-    // buttons: true,
     confirmButtonText: '다시 입력하기',
     });
-  } else {
+  } else if(($email == userId) || ($password == userPassword) || ($nickName == userNickName) || ($address == userAddress) || ($phoneNumber == userPhoneNumber)){
+    Swal.fire({
+      icon: 'error',
+      title: '중복 오류',
+      text: '다른 사용자와 중복된 정보가 있습니다!',
+      confirmButtonText: '다시 입력하기',
+      });
+  } else if(!(($email == '') || ($password == '') || ($nickName == '') || ($address == '') || ($phoneNumber == ''))) {
     Swal.fire({
     icon: 'success',                         
     title: '회원가입 성공',         
     text: '가입한 이메일과 비밀번호를 사용하여 다시 로그인 해주세요!',
-    // buttons: true,
     confirmButtonText: '확인',
     closeOnClickOutside : false 
     }).then(function moveToLogin() {
       location.href = '../login/login.html';
     });
   }
+  
+  
 });
