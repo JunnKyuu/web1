cf926f702a212ec0ae93b69b8a9cc9f1ced1a446
JunnKyuu <junnkyuu22@gmail.com>
2023-06-28 22:12:38 +0900

    react-practice1

---
+++ front_end/study-react/react-winterlood/react-practice1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name = '준규';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트! {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
