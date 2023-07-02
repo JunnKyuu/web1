4101fcff6df5989cec8f39ed2cd73d2f38c2a699
JunnKyuu <junnkyuu22@gmail.com>
2023-07-02 21:32:08 +0900

    filter btn 추가

--- front_end/study-react/todolist/script.js
+++ front_end/study-react/todolist/script.js
@@ -1,9 +1,39 @@
 const $addBtn = document.querySelector('#add-button');
 const $todoInput = document.querySelector('#todo-input');
 const $todoUl = document.querySelector('#todo-list');
+const $body = document.querySelector('body');
 
-let todos = [];
-let id = 0;
+let todos = []; // todo 배열
+let id = 0; // 구분하기 위한 id
+
+// btn container, btn 추가
+const createBtnContainer = () => {
+  const showBtnContainer = document.createElement('div');
+  showBtnContainer.classList.add('showBtnContainer');
+  $body.appendChild(showBtnContainer);
+
+  const createBtns = () => {
+    const allBtn = document.createElement('button');
+    allBtn.classList.add('allbtn');
+    allBtn.innerText = 'All';
+  
+    const toDoBtn = document.createElement('button');
+    toDoBtn.classList.add('toDoBtn');
+    toDoBtn.innerText = 'Todos';
+
+    const doneBtn = document.createElement('button');
+    doneBtn.classList.add('doneBtn');
+    doneBtn.innerText = 'Done';
+  
+     showBtnContainer.appendChild(allBtn);
+    showBtnContainer.appendChild(toDoBtn);
+    showBtnContainer.appendChild(doneBtn);
+  }
+
+  createBtns();
+}
+
+createBtnContainer();
 
 // enter 입력하면 todo추가
 const enterToAdd = () => {
@@ -13,12 +43,11 @@ const enterToAdd = () => {
         alert('할 일을 입력하세요!');
       } else {
         appendTodos(event.target.value);
-      $todoInput.value = '';
+        $todoInput.value = '';
       }
     }
   });
 }
-
 enterToAdd();
 
 // click하면 todo 추가
@@ -30,17 +59,19 @@ const clickToAdd = () => {
     $todoInput.value = '';
   }
 }
-
 $addBtn.addEventListener('click', () => clickToAdd($todoInput.value));
 
+// todo 만들기
 const setTodos = (newTodos) => {
   todos = newTodos;
 }
 
+// todo 가져오기
 const getTodos = () => {
   return todos;
 }
 
+// todo에 입력한 내용 추가
 const appendTodos = (text) => {
   const newId = id++;
   const newTodos = getTodos().concat({
@@ -53,18 +84,21 @@ const appendTodos = (text) => {
   showTodos();
 }
 
+// 삭제
 const deleteTodo = (todoId) => {
   const newTodos = getTodos().filter(todo => todo.id !== todoId);
   setTodos(newTodos);
   showTodos();
 }
 
+// 한 일 체크
 const doneTodo = (todoId) => {
   const newTodos = getTodos().map(todo => todo.id === todoId ? {...todo, isDone: !todo.isDone} : todo);
   setTodos(newTodos);
   showTodos();
 }
 
+// 화면에 todo 보여주기
 const showTodos = () => {
   $todoUl.innerHTML = '';
   const allTodos = getTodos();
@@ -89,7 +123,7 @@ const showTodos = () => {
 
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('deleteBtn');
-    deleteBtn.innerHTML = '삭제';
+    deleteBtn.innerHTML = '제거';
 
     deleteBtn.addEventListener('click', () => deleteTodo(todo.id));
 
