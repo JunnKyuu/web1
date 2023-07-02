73d7b0a922f7dc7001c6b87b278fcfb7f0f1d4d7
JunnKyuu <junnkyuu22@gmail.com>
2023-07-02 19:14:27 +0900

    add todo js

--- front_end/study-react/todolist/script.js
+++ front_end/study-react/todolist/script.js
@@ -1,77 +1,78 @@
 const $addBtn = document.querySelector('#add-button');
-const $todoUl = document.querySelector('#todo-list');
 const $todoInput = document.querySelector('#todo-input');
+const $todoUl = document.querySelector('#todo-list');
 
-const completeBtn = document.createElement('button');
-let todoArr = [];
+let todos = [];
+let id = 0;
 
-$addBtn.addEventListener('click', () => {
-  if($todoInput.value === '') {
-    alert('할 일을 입력해주세요!');
-  } else {
+// enter 입력하면 todo추가
+const init = () => {
+  $todoInput.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+      appendTodos(event.target.value);
+      $todoInput.value = '';
+    }
+  });
+}
+
+init();
+
+// 추가 클릭하면 todo추가
+// $addBtn.addEventListener('click', () => {
+//   const todoLi = document.createElement('li');
+//   todoLi.innerText = $todoInput.value;
+
+//   $todoUl.appendChild(todoLi);
+// });
+
+const setTodos = (newTodos) => {
+  todos = newTodos;
+}
+
+const getTodos = () => {
+  return todos;
+}
+
+const appendTodos = (text) => {
+  const newId = id++;
+  const newTodos = getTodos().concat({
+    id: newId,
+    isDone: false,
+    content: text
+  });
+
+  setTodos(newTodos);
+  paintTodos();
+}
+
+const paintTodos = () => {
+  $todoUl.innerHTML = '';
+  const allTodos = getTodos();
+
+  allTodos.forEach((todo) => {
     const todoLi = document.createElement('li');
-    todoLi.setAttribute('id', $todoInput.value);
+    todoLi.classList.add('todo-li');
 
-    const liContainer = document.createElement('div');
-    liContainer.style.width = '68vw';
-    liContainer.style.height = '55px';
-    liContainer.style.display = 'flex';
-    liContainer.style.flexDirection = 'row';
-    liContainer.style.alignItems = 'center';
-    liContainer.style.justifyContent = 'space-between';
-    liContainer.style.color = '#00c3ff';
-    liContainer.style.fontWeight = '700';
-    liContainer.style.fontSize = '15px';
-    liContainer.style.backgroundColor = '#ebfaff';
-    liContainer.style.borderRadius = '5px';
-    liContainer.style.marginTop = '15px';
+    const checkBox = document.createElement('div');
+    checkBox.classList.add('checkbox');
 
-
-    const liText = document.createElement('h1');
-    liText.innerText = $todoInput.value;
-    liText.style.marginLeft = '30px';
-    liContainer.appendChild(liText);
-
-    const btnContainer = document.createElement('div');
-    btnContainer.style.width = '120px';
-    btnContainer.style.height = '55px';
-    btnContainer.style.display = 'flex';
-    btnContainer.style.flexDirection = 'row';
-    btnContainer.style.alignItems = 'center';
-    btnContainer.style.justifyContent = 'space-between';
-    btnContainer.style.marginRight = '30px';
-    liContainer.appendChild(btnContainer);
-
-    const completeBtn = document.createElement('button');
-    completeBtn.style.width = '50px';
-    completeBtn.style.height = '30px';
-    completeBtn.style.color = 'white';
-    completeBtn.style.backgroundColor = '#35c5f0';
-    completeBtn.style.borderRadius = '5px';
-    completeBtn.style.border = 'none';
-    completeBtn.style.cursor = 'pointer';
-    completeBtn.innerText = '완료';
-
-    btnContainer.appendChild(completeBtn);
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todoItem');
+    todoItem.innerText = todo.content;
 
     const deleteBtn = document.createElement('button');
-    deleteBtn.style.width = '50px';
-    deleteBtn.style.height = '30px';
-    deleteBtn.style.color = 'white';
-    deleteBtn.style.backgroundColor = '#35c5f0';
-    deleteBtn.style.borderRadius = '5px';
-    deleteBtn.style.border = 'none';
-    deleteBtn.style.cursor = 'pointer';
-    deleteBtn.innerText = '삭제';
+    deleteBtn.classList.add('deleteBtn');
+    deleteBtn.innerHTML = 'X';
 
-    btnContainer.appendChild(deleteBtn);
+    if(todo.isDone === true) {
+      todoItem.classList.add('done');
+      checkBox.innerText = '✔';
+    }
 
+    todoLi.appendChild(checkBox);
+    todoLi.appendChild(todoItem);
+    todoLi.appendChild(deleteBtn);
 
-    todoLi.appendChild(liContainer);
-
-    $todoUl.appendChild(todoLi);
-
-    todoArr.push($todoInput.value);
-    console.log($todoInput.value);
-  }
-});
+   $todoUl.appendChild(todoLi);
+  });
+}
