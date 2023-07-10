787698ba74738a4b81f38b325614f474bac10146
JunnKyuu <junnkyuu22@gmail.com>
2023-07-10 12:53:28 +0900

    virtual DOM 복습 및 주석 추가

--- front_end/study-react/do-it-react/ch02/ch02_1/src/index.tsx
+++ front_end/study-react/do-it-react/ch02/ch02_1/src/index.tsx
@@ -5,8 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const pVirtualDOM = React.createElement('p', null, 'Hello Virtual DOM World!');
+// virtual DOM 생성
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// virtual DOM 연결할 DOM 설정
 
 root.render(pVirtualDOM);
+// virtual DOM 연결
 
 reportWebVitals();
