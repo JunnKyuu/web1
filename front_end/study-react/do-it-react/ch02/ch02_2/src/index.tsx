94f6f21e7f5c55c1b167782bb7c24b12d4a655e5
JunnKyuu <junnkyuu22@gmail.com>
2023-07-12 17:21:23 +0900

    JSX 구문 연습 - Array

--- front_end/study-react/do-it-react/ch02/ch02_2/src/index.tsx
+++ front_end/study-react/do-it-react/ch02/ch02_2/src/index.tsx
@@ -1,14 +1,28 @@
 import ReactDOM from 'react-dom/client';
 
-const rootVirtualDOM = (
+const rootVirtualDOM = [
   <ul>
     <li>
       <a href="http://www.google.com" target="_blank">
         <p>Go to Google!</p>
       </a>
     </li>
+  </ul>,
+  <ul>
+    <li>
+      <a href="http://www.naver.com" target="_blank">
+        <p>Go to Naver!</p>
+      </a>
+    </li>
+  </ul>,
+  <ul>
+    <li>
+      <a href="http://www.daum.net" target="_blank">
+        <p>Go to Daum!</p>
+      </a>
+    </li>
   </ul>
-);
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(rootVirtualDOM);
