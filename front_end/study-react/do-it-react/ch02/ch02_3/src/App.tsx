10a29ddb7a92ab215a7c180dea82f540a929da3e
JunnKyuu <junnkyuu22@gmail.com>
2023-07-14 13:13:44 +0900

    function Component 연습

--- front_end/study-react/do-it-react/ch02/ch02_3/src/App.tsx
+++ front_end/study-react/do-it-react/ch02/ch02_3/src/App.tsx
@@ -1,15 +1,23 @@
-import {Component} from 'react';
-import ClassComponent from './ClassComponent';
+// import {Component} from 'react';
+// import ClassComponent from './ClassComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <ul>
-        <ClassComponent />
-        <ClassComponent />
-      </ul>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <ul>
+//         <ClassComponent />
+//         <ClassComponent />
+//       </ul>
+//     );
+//   }
+// }
+
+// export default App;
+
+import {Component} from 'react';
+
+function App() {
+  return <h1>function Component</h1>;
 }
 
 export default App;
