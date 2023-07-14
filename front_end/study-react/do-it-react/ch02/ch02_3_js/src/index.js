6afb9e5aa804569cc1f80fec5b154dd22c8ab8d8
JunnKyuu <junnkyuu22@gmail.com>
2023-07-14 21:33:50 +0900

    class Component state, lifecycle

---
+++ front_end/study-react/do-it-react/ch02/ch02_3_js/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NotificationList from "./NotificationList";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NotificationList />
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
