12def722713dcd96d6a1ff94609ea21c41f3c74d
JunnKyuu <junnkyuu22@gmail.com>
2023-07-15 12:04:26 +0900

    React에서 Event 다루기

---
+++ front_end/study-react/react-soaple/my-app/src/chapter_08/ConfirmButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ConfirmButton() {
+  const [isConfirmed, setIsConfirmed] = React.useState(false);
+
+  const handleConfirm = () => {
+    setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
+  };
+
+  return (
+    <button onClick={handleConfirm} disabled={isConfirmed}>
+      {isConfirmed ? "확인됨" : "확인하기"}
+    </button>
+  );
+}
+
+export default ConfirmButton;
--- front_end/study-react/react-soaple/my-app/src/index.js
+++ front_end/study-react/react-soaple/my-app/src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ConfirmButton";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
